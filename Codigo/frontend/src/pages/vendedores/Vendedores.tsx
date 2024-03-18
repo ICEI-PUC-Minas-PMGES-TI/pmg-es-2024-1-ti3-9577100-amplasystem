257482e5ded945682a84b14e1d8362099de076a0
Pedro Henrique Pires Rodrigues <67903358+Credosvaldo@users.noreply.github.com>
2024-03-18 18:17:03 -0300

    dev: implement tabela

--- Codigo/frontend/src/pages/vendedores/Vendedores.tsx
+++ Codigo/frontend/src/pages/vendedores/Vendedores.tsx
@@ -10,12 +10,25 @@ import AddIcon from '@mui/icons-material/Add';
 import apiFetch from '../../services/api';
 import { VendedorModel } from 'models/VendedorModel';
 import { Box } from '@mui/system';
-import { Fab } from '@mui/material';
+import { Autocomplete, Button, Fab, Modal, TextField, Typography } from '@mui/material';
+import constructWithOptions from 'styled-components/dist/constructors/constructWithOptions';
+import { useRef } from 'react';
+
 
 const VendedoresPage = () => {
     const [data, setData] = React.useState<[VendedorModel]>([{}]);
+    const [open, setOpen] = React.useState(false)
 
     React.useEffect(() => {
+        getVendedores()
+    }, []);
+
+
+    function ChangeModalState() {
+        setOpen(!open);
+    }
+
+    function getVendedores() {
         apiFetch
             .get('/vendedor')
             .then((data) => {
@@ -24,7 +37,42 @@ const VendedoresPage = () => {
             .catch((e) => {
                 console.log(e);
             });
-    }, []);
+    }
+
+    const MODAL_STYLE = {
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%,-50%)',
+        padding: '150px',
+        backgroundColor: '#fff',
+        borderRadius: '10px',
+        color: 'black'
+    }
+
+    const optionCargo = ["ADIMINISTRADOR", "VENDEDOR"]
+
+    const refNome = useRef<HTMLInputElement>(null);
+    const refEmail = useRef<HTMLInputElement>(null);
+    const [refCargo, setRefCargo] = React.useState("")
+
+    function onSubmit() {
+        const obj = {
+            "nome": refNome.current?.value || '',
+            "email": refEmail.current?.value || '',
+            "cargo": refCargo
+        }
+
+        apiFetch.post("/vendedor/admin/save", obj)
+            .then(data => {
+                console.log(data)
+                getVendedores();
+            })
+
+
+    }
+
+
 
     return (
         <Box display={'flex'}>
@@ -50,9 +98,91 @@ const VendedoresPage = () => {
                     </TableBody>
                 </Table>
             </TableContainer>
-            <Fab color="primary" aria-label="add">
+            <Fab onClick={ChangeModalState} color="primary" aria-label="add">
                 <AddIcon />
             </Fab>
+
+            <Modal
+                open={open}
+                aria-labelledby="parent-modal-title"
+                aria-describedby="parent-modal-description"
+            >
+                <Box sx={{ ...MODAL_STYLE }}>
+                    <h2 id="parent-modal-title">Cadastrar vendedor</h2>
+
+                    <TextField
+                        id="nome"
+                        label="Nome"
+                        variant="outlined"
+                        placeholder="Nome"
+                        fullWidth
+                        margin="normal"
+                        sx={{
+                            borderRadius: '8px',
+                            maxWidth: 720,
+                            height: 65,
+                        }}
+                        inputRef={refNome}
+                    />
+
+                    <TextField
+                        id="email"
+                        label="Email"
+                        variant="outlined"
+                        placeholder="Email"
+                        fullWidth
+                        margin="normal"
+                        sx={{
+                            borderRadius: '8px',
+                            maxWidth: 720,
+                            height: 65,
+                        }}
+                        inputRef={refEmail}
+                    />
+
+                    <Autocomplete
+                        disablePortal
+                        id="cargo"
+                        options={optionCargo}
+                        sx={{ width: 300 }}
+                        renderInput={(params) => <TextField {...params} label="Cargo" />}
+                        onSelect={(event) => {
+                            setRefCargo(event.target.value)
+                        }}
+                    />
+
+                    <Button
+                        onClick={onSubmit}
+                        variant="contained"
+                        sx={{
+                            mt: 2,
+                            maxWidth: 720,
+                            backgroundColor: '#45BCEF',
+                            width: '100%',
+                            height: 55,
+                        }}
+                    >
+                        Confirmar
+                    </Button>
+
+                    <Button
+                        onClick={ChangeModalState}
+                        variant="contained"
+                        sx={{
+                            mt: 2,
+                            maxWidth: 720,
+                            backgroundColor: '#45BCEF',
+                            width: '100%',
+                            height: 55,
+                        }}
+                    >
+                        Cancelar
+                    </Button>
+
+                    <button onClick={ChangeModalState}>Fechar</button>
+                </Box>
+            </Modal>
+
         </Box>
     );
 };
--- Codigo/frontend/src/routes/Routes.tsx
+++ Codigo/frontend/src/routes/Routes.tsx
@@ -2,12 +2,13 @@ import React, { Suspense } from 'react';
 import { Navigate, Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import ProtectedRoute from '../routes/ProtectedRoutes';
 import { DashboardLayout } from '../layouts/DashboardLayout.tsx';
+import IndustriaPage from '../pages/industrias/Industria.tsx';
 
 const DashboardPage = React.lazy(() => import('../pages/dashboard/Dashboard'));
 const VendedoresPage = React.lazy(() => import('../pages/vendedores/Vendedores'));
 const LoginPage = React.lazy(() => import('../pages/login/LoginPage'));
 const ForgotPasswordGetEmail = React.lazy(() => import('../pages/forgotPassword/ForgotPasswordGetEmail'));
-const ForgotPasswordGetNewPassword = React.lazy(() => import('../pages/forgotPassword/ForgotPasswordGetNewPassword'));
+const ForgotPasswordGetNewPassword = React.lazy(() => import('../pages/forgotPassword/ForgotPasswordGetNewPassword.tsx'));
 const RouterRoutes = () => {
     return (
         <Router>
@@ -29,6 +30,14 @@ const RouterRoutes = () => {
                             element={
                                 <DashboardLayout>
                                     <VendedoresPage />
+                                </DashboardLayout>
+                            }
+                        />
+                        <Route
+                            path="/industrias"
+                            element={
+                                <DashboardLayout>
+                                    <IndustriaPage />
                                 </DashboardLayout>
                             }
                         />
