f7f358a333ea9730fc0d760300f90c2645814f14
LuisBrescia <luisfelipebrescia@gmail.com>
2024-03-18 12:30:25 -0300

    Adicionado table e requisição API na tela de Vendedores

--- Codigo/frontend/src/pages/vendedores/Vendedores.tsx
+++ Codigo/frontend/src/pages/vendedores/Vendedores.tsx
@@ -1,53 +1,61 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import { styled, ThemeProvider, createTheme } from '@mui/material/styles';
-import Divider from '@mui/material/Divider';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import ArrowRight from '@mui/icons-material/ArrowRight';
-import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
-import Home from '@mui/icons-material/Home';
-import Settings from '@mui/icons-material/Settings';
-import Dashboard from '@mui/icons-material/Dashboard';
-import Handshake from '@mui/icons-material/Handshake';
-import Factory from '@mui/icons-material/Factory';
-import { useNavigate } from 'react-router-dom';
 
-const data = [
-    { icon: <Dashboard />, label: 'Dashboard', location: '/dashboard' },
-    { icon: <Handshake />, label: 'Vendedor', location: '/vendedores' },
-    { icon: <Factory />, label: 'Indústria', location: '/industrias' },
+import apiFetch from '../../services/api';
+
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-const FireNav = styled(List)<{ component?: React.ElementType }>({
-    '& .MuiListItemButton-root': {
-        paddingLeft: 24,
-        paddingRight: 24,
-    },
-    '& .MuiListItemIcon-root': {
-        minWidth: 0,
-        marginRight: 16,
-    },
-    '& .MuiSvgIcon-root': {
-        fontSize: 20,
-    },
-});
+const VendedoresPage = () => {
+    React.useEffect(() => {
+        apiFetch('vendedores').then((data) => {
+            console.log(data);
+        });
+    }, []);
 
-import { PaletteMode, TableContainer } from '@mui/material';
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Dessert (100g serving)</TableCell>
+                        <TableCell align="right">Calories</TableCell>
+                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.calories}</TableCell>
+                            <TableCell align="right">{row.fat}</TableCell>
+                            <TableCell align="right">{row.carbs}</TableCell>
+                            <TableCell align="right">{row.protein}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
 
-const currentMode: PaletteMode = 'dark';
-const primaryColor = 'rgb(102, 157, 246)';
-// const secondaryColor = 'rgb(71, 98, 130)';
-const backgroundColor = 'rgb(5, 30, 52)';
-
-export default function CustomizedList() {
-    const [open, setOpen] = React.useState(true);
-    const navigate = useNavigate();
-    return <div />;
-}
+export default VendedoresPage;
