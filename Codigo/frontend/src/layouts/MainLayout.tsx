aa5ea45908123f343091c2877f9bab32e3092da0
Pedro Vilaça <pedrovilacav@gmail.com>
2024-03-17 22:25:30 -0300

    Criado layout inicial para tela de login

--- Codigo/frontend/src/layouts/MainLayout.tsx
+++ Codigo/frontend/src/layouts/MainLayout.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from 'react-router-dom';
-import Header from '../layouts/Header';
 import { Suspense } from 'react';
-import CustomLoader from '../lib/loader';
 
 export default function Layout() {
     return (
         <>
-            <Header />
-            <Suspense fallback={<CustomLoader />}>
+            {/*<Header />*/}
+            <Suspense fallback={<></>}>
+                {/* TODO: add a custom load => <Suspense fallback={<></> <CustomLoader />}>*/}
                 <Outlet />
             </Suspense>
         </>
