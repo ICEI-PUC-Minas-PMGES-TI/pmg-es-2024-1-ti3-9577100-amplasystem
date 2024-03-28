a649ac39838ad66a528a9552b1154cb9fb9d6030
victorreiscarlota <victor.reis.carlotta@gmail.com>
2024-03-28 09:28:54 -0300

    fix: corrigido os erros de importacao do tailwind

--- Codigo/frontend/src/layouts/DashboardLayout.tsx
+++ Codigo/frontend/src/layouts/DashboardLayout.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable */
+// eslint-disable-next-line prettier/prettier
+
 import Grid from '@mui/material/Grid';
-import Aside from '../components/Aside';
+import Sidebar, { SidebarItem } from '../components/Sidebar';
 import { Box, width } from '@mui/system';
+import { Calendar, Flag, Home, Layers, LayoutDashboard, LifeBuoy, Settings, StickyNote } from 'lucide-react';
 
 interface DashboardLayoutProps {
     children: React.ReactNode;
@@ -9,8 +13,17 @@ interface DashboardLayoutProps {
 export const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
     return (
         <Box display={'flex'}>
-            <Aside />
-
+            <Sidebar>
+                <SidebarItem icon={<Home size={20}/>} text="Home" alert/>
+                <SidebarItem icon={<LayoutDashboard size={20}/>} text="Dashboard" active/>
+                <SidebarItem icon={<StickyNote size={20}/>} text="Projects" alert/>
+                <SidebarItem icon={<Calendar size={20}/>} text="Calendar"/>
+                <SidebarItem icon={<Layers size={20}/>} text="Tasks" />
+                <SidebarItem icon={<Flag size={20}/>} text="Reporting" />
+                <hr className="my-3"/>
+                <SidebarItem icon={<Settings size={20}/>} text="Settings" />
+                <SidebarItem icon={<LifeBuoy size={20}/>} text="Help" />
+            </Sidebar>
             <main style={{ width: '90%', margin: '20px', maxHeight: '100vh' }}>{children}</main>
         </Box>
     );
