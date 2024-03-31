c9cee6a23d0e7c198f8cf399ecfe8e1dfe0be330
Pedro Vilaça <pedrovilacav@gmail.com>
2024-03-31 18:33:28 -0300

    renomeando variaveis

--- Codigo/backend/src/main/java/amplasystem/api/controller/ClienteController.java
+++ Codigo/backend/src/main/java/amplasystem/api/controller/ClienteController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/clientes")
+@RestController("Cliente")
+@RequestMapping("/cliente")
 public class ClienteController {
 
     private final ClienteService clienteService;
