0da1e463f4c6d5610adadf186de89fd7f1379f23
LuisBrescia <luisfelipebrescia@gmail.com>
2024-05-06 16:06:06 -0300

    Alterado aside para ter cores pretas

--- Codigo/frontend/src/components/Sidebar.tsx
+++ Codigo/frontend/src/components/Sidebar.tsx
@@ -149,8 +149,8 @@ const SidebarContext = createContext<SidebarContextType>({
 
 const StyledBox = styled(Box)(({ theme }) => ({
     // backgroundColor: theme.palette.secondary.dark,
-    backgroundColor: theme.palette.secondary.main, 
-    color: theme.palette.secondary.contrastText, 
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.secondary.contrastText,
     height: '100%',
 }));
 
@@ -187,17 +187,27 @@ export function SidebarItem({ icon, text, active, alert, location }: SidebarItem
             onClick={() => {
                 navigate(location);
             }}
-            className={`relative flex items-center py-2 px-3 my-1 font-medium rounded cursor-pointer transition-colors group 
-        ${active ? 'bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-600' : 'hover:bg-indigo-50 text-white hover:text-gray-500'}`}
+            className={`relative flex items-center py-2 px-3 my-1 font-medium rounded cursor-pointer group hover:outline outline-2 outline-neutral-500
+        ${active ?
+                    'bg-gradient-to-tr text-neutral-50 bg-neutral-800 hover:bg-neutral-700 hover:text-white' :
+                    'hover:bg-neutral-900 text-neutral-300 hover:text-neutral-100  '
+                }`}
         >
             {icon}
-            <span className={`overflow-hidden transition-all ${expanded ? 'w-52 ml-3' : 'w-0'}`}>{text}</span>
+            <span className={`overflow-hidden ${expanded ? 'w-52 ml-3' : 'w-0'}`}
+                style={{
+                    transitionProperty: 'width, margin-left',
+                    transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+                    transitionDuration: '150ms'
+                }}>
+                {text}
+            </span>
             {alert && <div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? '' : 'top-2 '}`} />}
 
             {!expanded && (
                 <div
                     className={`absolute left-full rounded px-2 py-1 ml-6 bg-indigo-100 z-50 text-indigo-800 text-sm invisible opacity-20
-                    -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}
+                    -translate-x-3 group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}
                 >
                     {text}
                 </div>
