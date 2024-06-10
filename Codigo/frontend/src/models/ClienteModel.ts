0a700cc45895041f1d8cda0594d3d7cab61005c8
LuisBrescia <luisfelipebrescia@gmail.com>
2024-06-10 01:04:37 -0300

    Adicionado nova logo e organizado ordem dos atributos de cliente

--- Codigo/frontend/src/models/ClienteModel.ts
+++ Codigo/frontend/src/models/ClienteModel.ts
@@ -4,17 +4,17 @@ import { EnderecoModel } from './EnderecoModel';
 export type ClienteModel = {
     id: number | null;
     cnpj: string;
+    nomeFantasia: string;
     telefone?: string;
     endereco: EnderecoModel;
-    nomeFantasia: string;
     vendedor: VendedorModel;
 };
 
 export type ClienteFormModel = {
     id: number | null;
-    nomeFantasia: string;
     cnpj: string;
-    idVendedor: number;
+    nomeFantasia: string;
     telefone?: string;
     endereco?: EnderecoModel;
+    idVendedor: number;
 };
